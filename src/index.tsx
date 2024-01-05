75ec3a504971b31b3282b867d382907b1bf93f9b
Marcus Will <marcusxwill@gmail.com>
2024-01-05 13:49:43 +0100

    add authorization

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
 import { render } from 'solid-js/web'
 
 import './root.scss'
-import App from './pages/App'
+import App from './pages/Intro'
 
 const root = document.getElementById('root')
 
---
+++ src/utils/encrypt.ts
@@ -0,0 +1,26 @@
+
+const generateRandomString = (length: number) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+}
+
+const sha256 = async (plain: string) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+
+const base64encode = (input: ArrayBufferLike) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
+
+export const codeVerifier = generateRandomString(64);
+
+export const codeChallenge = async (): Promise<string> => {
+    const hashed = await sha256(codeVerifier)
+    return base64encode(hashed);
+}
