5014f277323a9bd581fcb0869581ef50392ee751
Marcus Will <marcusxwill@gmail.com>
2024-03-06 16:53:53 +0100

    add playlists

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,12 +9,13 @@ function Dashboard() {
     string | undefined | null
   >();
   const [topTracks, setTopTracks] = createSignal<string | undefined | null>();
+  const [playlists, setPlaylists] = createSignal<string | undefined | null>();
 
   if (!accessToken()) {
     setAccessToken(window.localStorage.getItem("access_token"));
   }
 
-  async function fetchWebApi(endpoint, method, body?) {
+  async function fetchWebApi(endpoint: string, method: string, body?: any) {
     const res = await fetch(`https://api.spotify.com/${endpoint}`, {
       headers: {
         Authorization: `Bearer ${accessToken()}`,
@@ -33,23 +34,34 @@ function Dashboard() {
     setTopTracks(topTracks.items);
   }
 
+  async function getMyPlaylists() {
+    const topTracks = await fetchWebApi(
+      "v1/me/playlists",
+      "GET"
+    );
+    setPlaylists(topTracks.items);
+  }
+
   console.log("render Dashboard");
   console.log(accessToken());
 
   return (
     <>
       <h1>Dashboard</h1>
+      <p>{accessToken()}</p>
       <Show when={accessToken()} fallback={<Login />}>
         <Card>
           <Button onClick={() => getTopTracks()}>get Top Tracks</Button>
+          <Button onClick={() => getMyPlaylists()}>get Top Tracks</Button>
           {topTracks() &&
             topTracks()?.map(
               ({ name, artists }) =>
-                `${name} by ${artists.map((artist) => artist.name).join(", ")}`
+                ` ${name} by ${artists.map((artist) => artist.name).join(", ")}`
             )}
         </Card>
+
       </Show>
-      <Login></Login>
+      {/* <Login></Login> */}
     </>
   );
 }
